fa937ed9a00a02faad00e663f5a3f118fb28e961
Betanofnts <betanobetano183@gmail.com>
2026-07-05 23:22:47 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    question: "Ce înseamnă Meta Gaming (MG)?",
+    answers: [
+      { text: "Folosirea informațiilor OOC în IC", correct: true },
+      { text: "Împușcarea unui jucător fără motiv", correct: false },
+      { text: "Înot pe distanțe lungi", correct: false },
+      { text: "Fuga de poliție", correct: false }
+    ]
+  },
+  {
+    question: "Ce este Fail RP?",
+    answers: [
+      { text: "Acțiuni nerealiste în roleplay", correct: true },
+      { text: "Respectarea regulamentului", correct: false },
+      { text: "Roleplay corect", correct: false },
+      { text: "Interacțiune OOC", correct: false }
+    ]
+  }
+];
